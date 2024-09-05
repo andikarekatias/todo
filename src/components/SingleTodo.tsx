f351414f9db0d05e4dbfc123932ec290b8e72cc2
andikarekatias <andika.rekatias@yahoo.com>
2024-09-05 08:50:29 +0700

    edit todo

--- src/components/SingleTodo.tsx
+++ src/components/SingleTodo.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Todo } from '../model'
 import IconPencilSquare from './svg/IconPencilSquare'
 import IconTrash from './svg/IconTrash'
@@ -10,6 +10,23 @@ type Props = {
     setTodos: React.Dispatch<React.SetStateAction<Todo[]>>
 }
 const SingleTodo = ({todo, todos, setTodos}: Props) => {
+
+    const [edit, setEdit] = React.useState<boolean>(false)
+    const [editTodo, setEditTodo] = React.useState<string>(todo.todo)    
+
+    const handleEdit = (e: React.FormEvent, id: number) => {
+        e.preventDefault()
+        setTodos(todos.map(todo => 
+            todo.id === id ? {
+                ...todo, 
+                todo: editTodo
+            } : todo))
+        setEdit(false)
+    }
+    const handleDelete = (id: number) => {
+        setTodos(todos.filter(todo => todo.id !== id))
+    }
+
     const handleDone = (id: number) => {
         setTodos(todos.map(todo => 
             todo.id === id ? {
@@ -18,27 +35,39 @@ const SingleTodo = ({todo, todos, setTodos}: Props) => {
             } : todo))
     }
 
-    const handleDelete = (id: number) => {
-        setTodos(todos.filter(todo => todo.id !== id))
-    }
+    const inputRef = React.useRef<HTMLInputElement>(null)
+
+    useEffect(() => {
+        inputRef.current?.focus()
+    }, [edit])
 
   return (
-    <form className='w-full md:w-[45%]'>
+    <form className='w-full md:w-[45%]' onSubmit={(e) => handleEdit(e, todo.id)}>
         <div className="card bg-base-100 image-full w-full shadow-xl m-1">        
             <div className="card-body grid grid-cols-6"> 
                 { 
-                    todo.isDone ? (
+                    edit ? (
+                        <input className='col-span-5 input w-full max-w-xs' ref={inputRef} value={editTodo} onChange={(e) => setEditTodo(e.target.value)}/>
+                    ) : todo.isDone ? (
                         <div className='col-span-5 line-through text-red-500'>
                             {todo.todo}
                         </div>
-                    ):(
+                    ) : (
                         <div className='col-span-5'>
                             {todo.todo}
                         </div>
                     )
                 }                           
-                <div className='col-span-1 flex justify-end'>
-                    <span className='cursor-pointer hover:text-primary'><IconPencilSquare className='size-5'/></span>
+                <div className='col-span-1 flex justify-end items-center'>
+                    <span className='cursor-pointer hover:text-primary'
+                        onClick={() => {
+                            if(!edit && !todo.isDone){
+                                setEdit(!edit)
+                            }                            
+                        }}
+                    >
+                        <IconPencilSquare className='size-5'/>
+                    </span>
                     <span className='cursor-pointer hover:text-error' onClick={() => handleDelete(todo.id)}><IconTrash className='size-5'/></span>
                     <span className='cursor-pointer hover:text-info' onClick={() => handleDone(todo.id)}><IconCheck className='size-5'/></span>
                 </div>
